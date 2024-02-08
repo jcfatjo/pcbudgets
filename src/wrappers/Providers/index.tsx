ca92aa6ee6f7e677a5b82cfdea37d084b31ca69f
José Carlos Fatjó García <jc.fatjo@outlook.com>
2024-02-08 21:21:47 +0100

    feat: add tanstack react-query.

--- src/wrappers/Providers/index.tsx
+++ src/wrappers/Providers/index.tsx
@@ -1,8 +1,15 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React, { type PropsWithChildren } from "react";
 
+const queryClient = new QueryClient();
+
 const Providers: React.FC<PropsWithChildren> = ({ children }) => {
-    return <ChakraProvider>{children}</ChakraProvider>;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ChakraProvider>{children}</ChakraProvider>
+        </QueryClientProvider>
+    );
 };
 
 export default Providers;
